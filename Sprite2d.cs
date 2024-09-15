8ab3b52af328eac89937d097f2b85673673926c9
Settledmonkey21 <Keviniglesias15@outlook.com>
2024-09-15 19:08:02 -0400

    test

---
+++ Sprite2d.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Sprite2d : Sprite2D
+{
+	
+	public override void _Ready()
+	{
+	}
+
+	
+	public override void _Process(double delta)
+	{
+		
+
+
+
+
+
+
+	}
+}
